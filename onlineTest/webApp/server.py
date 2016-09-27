e16a3d06b40472f6a3400fc016be5503ef9eb889
gaurav <gagu12ec@gmail.com>
2016-09-27 10:56:59 +0530

    root values has been changed

--- onlineTest/webApp/server.py
+++ onlineTest/webApp/server.py
@@ -1,15 +1,27 @@
 import flask
+from flask import Flask
 
 
 # Create the application.
-APP = flask.Flask(__name__)
+APP = Flask(__name__)
+
+
+
 
 
 @APP.route('/')
 def index():
     """ Displays the index page accessible at '/'
     """
-    return flask.render_template('login_page_student.html')
+    return flask.render_template('welcome_student.html')
+
+@APP.route('/admin')
+def index1 ():
+    """ Displays the index page accessible at '/'
+    """
+    return flask.render_template('login_page_admin.html')
+
+
 
 
 if __name__ == '__main__':
