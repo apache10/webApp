29af4b6521d791f6fbed4ca65a92cc9618a58b43
gaurav <gagu12ec@gmail.com>
2016-09-29 10:21:42 +0530

    admin dashboard is connected to admin login

--- onlineTest/webApp/Server.py
+++ onlineTest/webApp/Server.py
@@ -4,25 +4,36 @@ from flask import Flask, request, render_template
 # Create the application.
 APP = Flask(__name__)
 
+
+
 @APP.route('/')
 def welcome ():
-    """ Displays the index page accessible at '/'
-    """
     return flask.render_template('welcome.html')
+
+
 
 @APP.route('/login-admin')
 def loginAdmin ():
- 
     return render_template('login_page_admin.html')
+
+
 
 @APP.route('/login-student')
 def loginStudent():
-
     return render_template('login_page_student.html')
+
+
+
     
-@APP.route('/dashboard-admin')
+@APP.route('/dashboard-admin',methods = ['POST', 'GET'])
 def adminDash():
-    return render_template('dashboard_admin.html')
+	if request.method == 'POST':
+		name=request.form['name']
+		email=request.form['email']
+    	return render_template('dashboard_admin.html', name=name, email=email)
+
+
+
 
 @APP.route('/dashboard-student')
 def studentDash():
