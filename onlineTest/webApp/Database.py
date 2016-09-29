36f813b59ed2667223624109e3a64f3915e06083
gaurav <gagu12ec@gmail.com>
2016-09-29 13:37:29 +0530

    connection with database has been created

--- onlineTest/webApp/Database.py
+++ onlineTest/webApp/Database.py
@@ -8,16 +8,28 @@ cursor.execute("DROP TABLE IF EXISTS STUDENT")
 sql = """CREATE TABLE STUDENT (
          FIRST_NAME  CHAR(20) NOT NULL,
          LAST_NAME  CHAR(20)NOT NULL,
-         EMAIL CHAR(20) NOT NULL,  
-         PASSWORD CHAR(20)NOT NULL)"""
+         EMAIL CHAR(20) NOT NULL,
+         PASSWORD CHAR(20)NOT NULL,
+         PRIMARY KEY(EMAIL))"""
 
 cursor.execute(sql)
+
 cursor.execute("DROP TABLE IF EXISTS ADMIN")
 
-sql = """CREATE TABLE ADMIN (
+sql1 = """CREATE TABLE ADMIN (
          FIRST_NAME  CHAR(20) NOT NULL,
          EMAIL CHAR(20) NOT NULL,  
          PASSWORD CHAR(20)NOT NULL)"""
 
-cursor.execute(sql)
+cursor.execute(sql1)
+
+cursor.execute("DROP TABLE IF EXISTS MARKS")
+
+sql2 = """CREATE TABLE MARKS (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         EMAIL CHAR(20) NOT NULL, 
+         SCORE CHAR(20)NOT NULL,
+         PRIMARY KEY(EMAIL))"""
+
+cursor.execute(sql2)
 db.close()
--- onlineTest/webApp/Server.py
+++ onlineTest/webApp/Server.py
@@ -1,5 +1,8 @@
 import flask
 from flask import Flask, request, render_template
+import MySQLdb
+db = MySQLdb.connect("localhost","root","pavilion@1","WEBAPP" )
+cursor = db.cursor()
 
 # Create the application.
 APP = Flask(__name__)
@@ -23,6 +26,21 @@ def loginStudent():
     return render_template('login_page_student.html')
 
 
+def checkAdmin(name,email,password):
+	sql = "SELECT * FROM ADMIN WHERE EMAIL = '%s'" % (email)
+	try:
+		cursor.execute(sql)
+		results = cursor.fetchall()
+		for row in results:
+			fname = row[0]
+			demail = row[1]
+			dpassword = row[2]
+	except:
+		print "Error: unable to fecth data"
+	if( dpassword == password):
+		return True
+	else:
+		return False
 
     
 @APP.route('/dashboard-admin',methods = ['POST', 'GET'])
@@ -30,14 +48,43 @@ def adminDash():
 	if request.method == 'POST':
 		name=request.form['name']
 		email=request.form['email']
-    	return render_template('dashboard_admin.html', name=name, email=email)
+		password=request.form['password']
+		if(checkAdmin(name,email,password)):
+			return render_template('dashboard_admin.html', name=name, email=email)
+		else:
+			return render_template('login_page_admin.html')
 
 
 
+def checkStudent(email,password):
+	print email
+	sql1 = "SELECT * FROM STUDENT WHERE EMAIL = '%s'" % (email)
+	try:
+		cursor.execute(sq1l)
+		results = cursor.fetchall()
+		for row in results:
+			fname= row[0]
+			lname = row[1]
+			demail = row[2]
+			dpassword = row[3]
+		print demail
+		print dpassword
+	except:
+		print "Error: unable to fecth data"
+	if( False):
+		return True
+	else:
+		return False
 
-@APP.route('/dashboard-student')
+@APP.route('/dashboard-student',methods = ['POST', 'GET'])
 def studentDash():
-    return render_template('dashboard_student.html')
+	if request.method == 'POST':
+		email=request.form['emailS']
+		password=request.form['passwordS']
+		if(checkStudent(email,password)):
+			return render_template('dashboard_student.html')
+		else:
+			return render_template('login_page_student.html')
 
 
 
